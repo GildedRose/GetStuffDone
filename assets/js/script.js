1e94bdfc09bb19c739177bb7b332a6514de788ef
Anne <anne.k.plummer@gmail.com>
2020-06-22 03:13:51 -0500

    color coded past present futre times

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,5 +8,19 @@ var i;
 
 for(i=9; i< 18; i++){
     var temp = ".row-" + i;
-    $(temp).append(moment(i, "HH").format('h:mm'));
-};
+    var hr = moment(i, "HH");
+
+    $(temp).append(hr.format('h:mm'));
+
+    var delta = hr.diff(moment().startOf('hour'));
+    var row = "#row-" + i;
+    if (delta > 0){
+        $(row).addClass("future")
+    }
+    else if (delta < 0){
+        $(row).addClass("past")
+    }
+    else {
+        $(row).addClass("present")
+    }
+}
