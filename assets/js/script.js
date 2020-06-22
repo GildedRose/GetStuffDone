513da48e87907ecfa13e400cd6359a26ee1bdbc2
Anne <anne.k.plummer@gmail.com>
2020-06-22 02:14:41 -0500

    added hour labels and moment hour for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@
 var now = moment().format('MMMM Do YYYY, h:mm:ss a');
 console.log(now);
 $('#currentDay').append(now);
+
+//time loop
+var i;
+
+for(i=9; i< 18; i++){
+    var temp = ".row-" + i;
+    $(temp).append(moment(i, "HH").format('h:mm'));
+};
